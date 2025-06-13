516f076c47850e9e6c4c6a8d5ccce82cd69f4a4c
Vikash0277 <prasadvikash308@gmail.com>
2025-06-13 18:25:01 +0530

    reslove reload issue

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import Balance from "../components/Balance"
 import Users from "../components/Users"
 import { Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
 
 export default function Dashboard() {
     return <div className="">
@@ -18,8 +19,29 @@ export default function Dashboard() {
 }
 
 const ProtectedRoute = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [auth, setAuth] = useState(false);
+
+  useEffect(() => {
     const token = localStorage.getItem("token");
-    return token ? children : <Navigate to="/signin" />;
+    if (token) {
+      setAuth(true);
+    }
+    setLoading(false);
+  }, []);
+
+  if (loading) return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+        </div>
+    );
+
+
+  return auth ? children : <Navigate to="/signin" />;
+};
+
+ProtectedRoute.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 ProtectedRoute.propTypes = {
